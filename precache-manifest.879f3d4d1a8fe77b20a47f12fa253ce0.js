ec06cddc1308a2115d849ecb8a0d9e52a6870580
Mina <minamoner01811@gmail.com>
2020-09-24 00:00:29 +0200

    first

---
+++ precache-manifest.879f3d4d1a8fe77b20a47f12fa253ce0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcad3997039741253a486c14076fe4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18647122d51eaf5f5d7b",
+    "url": "/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "baa917d483cc49cf86a5",
+    "url": "/static/js/2.07d5477f.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/static/js/2.07d5477f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18647122d51eaf5f5d7b",
+    "url": "/static/js/main.9e8a7b2b.chunk.js"
+  },
+  {
+    "revision": "2d0c8c2e2ef3e158a768",
+    "url": "/static/js/runtime-main.688998fc.js"
+  },
+  {
+    "revision": "2d5f75220b9ce839f6e7ea0f7de2d670",
+    "url": "/static/media/Home.2d5f7522.JPG"
+  },
+  {
+    "revision": "314e6a88ac2cfaa9081399bccf2446af",
+    "url": "/static/media/joe.314e6a88.png"
+  },
+  {
+    "revision": "da6e18558c3205775dd3a5b0df785ee7",
+    "url": "/static/media/joe1.da6e1855.jpeg"
+  },
+  {
+    "revision": "1cd7e892551b648359317eba2e6b21ea",
+    "url": "/static/media/joe2.1cd7e892.jpeg"
+  },
+  {
+    "revision": "2a5a7ae143c279032384e40965e9397a",
+    "url": "/static/media/joebg2.2a5a7ae1.jpg"
+  }
+]);
